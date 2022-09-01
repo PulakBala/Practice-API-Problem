396ae8520cca21c98e2aa5d174b9b38730cee8ab
PulakBala <pulakbala0524@gmail.com>
2022-09-02 00:12:46 +0600

    Problem1,2,3,4

--- Problem-1 api/main.js
+++ Problem-1 api/main.js
@@ -1,11 +1,36 @@
 /*
 ১. একদম প্রাথমিক স্টেপ হিসেবে jsonplaceholder এর ওয়েবসাইট থেকে ডাটা fetch করে সেটাকে কনসোল এ দেখাতে হবে। ধরো তুমি তাদের ওয়েবসাইট এ comments এর API এর লিংক কোনটা সেটা জাভাস্ক্রিপ্ট দিয়ে কোড করে সেই ডাটা কনসোল এ দেখতে পারতেছো কিনা। 
+
+
+২. যে কমেন্ট এর ডাটাগুলো কনসোল এ দেখাতে পারছো। সেগুলা আবার তুমি html এ দেখাতে পারতেছো কিনা। একটা সিস্টেম হবে তোমার একটা বাটন। থাকবে সেটাতে ক্লিক করলে ডাটা লোড হবে। তারপর সেই ডাটা তুমি ওয়েবসাইট এ দেখাবে। 
+
+৩. আরেকটা সিস্টেম হবে। তুমি যে ডাটা লোড করেছো। সেটা কোন বাটনে ক্লিক করা লাগবে না। ওয়েবসাইট লোড হলেই সরাসরি ডাটা লোড হয়ে তারপর সেই ডাটা ওয়েবসাইট এ দেখাবে। চেষ্টা করবে লোড করা ডাটা এর দুইটা প্রপার্টি অবশ্যই দেখাবে। 
+
+৪. তুমি যে ডাটা লোড করেছো। বা ডাটা ওয়েবসাইট এ দেখাচ্ছ। সেই কোড এর মধ্যে arrow ফাংশন ইউজ করতে পারতেছো কিনা। যখন লুপ চালাচ্ছ সেখানে forEach ইউজ করতে পারতেছো কিনা। 
 */
 
 const loadComments = () => {
     const url = 'https://jsonplaceholder.typicode.com/comments';
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayComments(data))
 }
-loadComments();
+
+const displayComments = (user) =>{
+    const commentContainer = document.getElementById('comments-container');
+    user.forEach(element => {
+        console.log(element);
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+            <h1>Id: ${element.id}</h1>
+            <p>Name: ${element.name}</p>
+            <p>Email: ${element.email}</p>
+        
+        `;
+        commentContainer.appendChild(newDiv);
+    });
+}
+
+    // loadComments();
+
+
